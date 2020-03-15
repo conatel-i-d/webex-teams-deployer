6803bc565d753cc43affdab6e08e96d22ad54f41
Guzman Monne <guzmonne@hotmail.com>
2020-03-14 22:52:44 -0300

    connected electron and react together

---
+++ electron/main.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var url = require('url');
+
+var { app, BrowserWindow, ipcMain } = require('electron');
+var { channels } = require('../src/shared/constants.js');
+
+var mainWindow;
+
+function createWindow() {
+  var startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '../index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  mainWindow.loadURL(startUrl);
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit());
+app.on('activate', () => mainWindow === null && createWindow());
+
+ipcMain.on(channels.APP_INFO, (event) => {
+  event.sender.send(channels.APP_INFO, {
+    appName: app.getName(),
+    appVersion: app.getVersion(),
+  });
+});
--- src/react/App.js
+++ src/react/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import { channels } from '../shared/constants.js';
+
+var { ipcRenderer } = window;
 
 function App() {
+  var [state, setState] = React.useState({ appName: '', appVersion: ''});
+
+  React.useEffect(() => {
+    ipcRenderer.send(channels.APP_INFO);
+    ipcRenderer.on(channels.APP_INFO, (event, arg) => {
+      ipcRenderer.removeAllListeners(channels.APP_INFO);
+      var { appName, appVersion } = arg;
+      setState({ appName, appVersion });
+    });
+  }, [setState]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +33,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>{state.appName} version {state.appVersion}</p>
       </header>
     </div>
   );
